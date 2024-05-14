d30195fa44c96b151366f0e11b5f9e2ea6312587
Pedro Santana <pmendes09@hotmail.com>
2024-05-13 21:06:10 -0300

    Dockerfile

--- shared/database.py
+++ shared/database.py
@@ -1,7 +1,13 @@
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/fastApi"
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "fallback_connection_string")
+
+print("SQLALCHEMY_DATABASE_URL:", SQLALCHEMY_DATABASE_URL)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
